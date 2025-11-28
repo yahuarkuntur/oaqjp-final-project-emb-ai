a952e264d2781cedb0e5da20171f997a7624ea9f
yahuarkuntur <debuirebrian@gmail.com>
2025-11-27 22:28:59 -0500

    Task 8: Run static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+'''
+    Deployment of the Flask app.
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +9,16 @@ app = Flask("Emotion Detection App")
 
 @app.route("/")
 def index_page():
+    '''
+        Renders the index page.
+    '''
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    '''
+        Analyzes the submited text and returns emotions data.
+    '''
     text_to_analyze = request.args.get("textToAnalyze")
     emotion_response = emotion_detector(text_to_analyze)
 
@@ -22,12 +32,13 @@ def detect_emotion():
 
     for key in emotion_keys:
         emotion_key_values.append(f"'{key}': {emotion_response[key]}")
-    
+
     last_emotion_key_value = emotion_key_values.pop()
+    dominant_emotion = emotion_response['dominant_emotion']
 
     response_text += ", ".join(emotion_key_values) + " and " + last_emotion_key_value
 
-    response_text += f". The dominant emotion is <strong>{emotion_response['dominant_emotion']}</strong>."
+    response_text += f". The dominant emotion is <strong>{dominant_emotion}</strong>."
 
     return response_text
 
